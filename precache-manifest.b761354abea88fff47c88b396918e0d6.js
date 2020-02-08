3b1e1e1eb9707eaad74233c247d6a137f017e9c0
ebbes <ebesojosephanyicoh@gmail.com>
2020-02-08 23:47:46 +0100

    Updates

---
+++ precache-manifest.b761354abea88fff47c88b396918e0d6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc4f1bfe26d3b1520d9bc0a4c79e1556",
+    "url": "/quizes/index.html"
+  },
+  {
+    "revision": "56fec96ce74829cde44c",
+    "url": "/quizes/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "622057ade8dcfa0acb41",
+    "url": "/quizes/static/js/2.ae853f9e.chunk.js"
+  },
+  {
+    "revision": "860bc5081fcd60d3c959d0d8166dd439",
+    "url": "/quizes/static/js/2.ae853f9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56fec96ce74829cde44c",
+    "url": "/quizes/static/js/main.0a6fa908.chunk.js"
+  },
+  {
+    "revision": "08aba48873a2f64911fa",
+    "url": "/quizes/static/js/runtime-main.889dca0b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/quizes/static/media/logo.5d5d9eef.svg"
+  }
+]);
